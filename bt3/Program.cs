8fc929b7f0d0ed0c2765f28a744dbb4117b3b0ce
ellie21520813 <21520813@gm.uit.edu.vn>
2023-03-25 23:25:08 +0700

    init prọect

---
+++ bt3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        server.Bind(iep);
+        //tao ra mot Endpot tu xa de nhan du lieu ve
+        IPEndPoint RemoteEp = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint remote = (EndPoint)RemoteEp;
+        byte[] data = new byte[1024];
+        int recv = server.ReceiveFrom(data, ref remote);
+        string s = Encoding.ASCII.GetString(data, 0, recv);
+        Console.WriteLine("Client:{0}", s);
+        data = Encoding.ASCII.GetBytes("Chao client");
+        server.SendTo(data, remote);
+        while (true)
+        {
+            data = new byte[1024];
+            recv = server.ReceiveFrom(data, ref remote);
+            s = Encoding.ASCII.GetString(data, 0, recv);
+            if (s.ToUpper().Equals("QUIT")) break;
+            Console.WriteLine(s); data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(s);
+            server.SendTo(data, 0, data.Length, SocketFlags.None, remote);
+        }
+        server.Close();
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+{
+    IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"),2008); 
+    Socket client = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp); 
+    String s = "Chao server";
+    byte[] data = new byte[1024]; 
+    data = Encoding.ASCII.GetBytes(s); client.SendTo(data, iep); 
+    EndPoint remote = (EndPoint)iep; 
+    data = new byte[1024];
+    int recv = client.ReceiveFrom(data, ref remote); s =Encoding.ASCII.GetString(data,0, recv);
+    Console.WriteLine(" Server{ 0}",s);
+    while (true)
+    {
+        s = Console.ReadLine();
+        data=new byte[1024];
+        data = Encoding.ASCII.GetBytes(s);
+        client.SendTo(data, remote);
+        if(s.ToUpper().Equals("QUIT"))
+            break; 
+        data = new byte[1024]; 
+        recv = client.ReceiveFrom(data, ref remote);
+        s =Encoding.ASCII.GetString(data,0, recv); 
+        Console.WriteLine(s);
+}
+client.Close();
+}
